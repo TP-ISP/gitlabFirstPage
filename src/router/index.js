efd37f8081d450ea473a2ef6d4c0e2c6de78a40a
zhangzju <zhangway19921221@163.com>
2017-10-18 02:41:52 +0800

    尝试切换路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Navigator from '@/components/Navigator'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -12,9 +13,9 @@ export default new Router({
       component: Navigator
     },
     {
-      path: '/',
-      name: 'Navigator',
-      component: Navigator
+      path: '/hello',
+      name: 'Hello',
+      component: HelloWorld
     }
   ]
 })
