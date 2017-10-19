378b39162a60d7dfc84d951193176322a4d96a34
zhangzju <zhangway19921221@163.com>
2017-10-20 02:36:09 +0800

    划分了顶部菜单和左侧导航栏组件

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,16 @@
 <template>
   <div id="app">
-    <el-col :span="4">
-    <h5>分组</h5>
-    <el-menu mode="vertical" default-active="1" class="el-menu-vertical-demo">
-      <el-menu-item-group title="分组一">
-        <el-menu-item index="1"><i class="el-icon-message"></i>导航一</el-menu-item>
-        <el-menu-item index="2"><i class="el-icon-message"></i>导航二</el-menu-item>
-      </el-menu-item-group>
-      <el-menu-item-group title="分组二">
-        <el-menu-item index="3"><i class="el-icon-message"></i>导航三</el-menu-item>
-        <el-menu-item index="4"><i class="el-icon-message"></i>导航四</el-menu-item>
-      </el-menu-item-group>
-    </el-menu>
-  </el-col>
+  <TopMenu></TopMenu>
+    <LeftSidebar></LeftSidebar>
     <router-view/>
   </div>
 </template>
 
 <script>
+  import LeftSidebar from './components/LeftSidebar'
+  import TopMenu from './components/TopMenu'
   export default {
+    components: {LeftSidebar, TopMenu},
     name: 'app'
   }
 </script>
